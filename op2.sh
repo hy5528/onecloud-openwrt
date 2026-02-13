e54f07e1b189677f4feca5dca2065e72d7815811
hy5528 <95360525+hy5528@users.noreply.github.com>
2026-02-14 06:24:33 +0800

    Update op2.sh

--- op2.sh
+++ op2.sh
@@ -11,6 +11,7 @@
 #
 
 # Modify default IP
+sed -i "s/192.168.1.1/192.168.2.158/" openwrt/package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.158/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
